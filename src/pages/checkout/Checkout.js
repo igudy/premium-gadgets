3385a7bec7d9d96cc8c8b18a6b518906c5c285fa
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-04-25 18:30:21 +0100

    api endpoint added

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -40,7 +40,7 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4242/create-payment-intent", {
+    fetch("https://igshop-api.onrender.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
