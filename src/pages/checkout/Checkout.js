3cad5d8c628b240b541a0da34316e2d6d3cac738
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-04-25 17:20:47 +0100

    Localhost fixed

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -40,7 +40,7 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("create-payment-intent", {
+    fetch("http://localhost:4242/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
