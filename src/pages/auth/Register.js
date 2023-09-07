1313cfa6898d0e95515fe254d92274ee1fb87c17
igudy <goodnessigunma1@gmail.com>
2023-09-07 16:47:25 +0100

    register page translation implemented

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -13,12 +13,15 @@ import { auth } from "../../firebase/config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 import Loader from "../../components/loader/Loader";
+import { useTranslation } from "react-i18next";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cPassword, setCPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const { t } = useTranslation();
 
   // Navigation
   const navigate = useNavigate();
@@ -49,36 +52,38 @@ const Register = () => {
       <section className={`container ${styles.auth}`}>
         <Card>
           <div className={styles.form}>
-            <h2>Register</h2>
+            <h2>{t("register")}</h2>
             <form onSubmit={registerUser}>
               <input
                 type="text"
-                placeholder="Email"
+                placeholder={t("email")}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
               <input
                 type="password"
-                placeholder="Password"
+                placeholder={t("password")}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
               <input
                 type="password"
-                placeholder="Confirm Password"
+                placeholder={t("confirm-password")}
                 value={cPassword}
                 onChange={(e) => setCPassword(e.target.value)}
                 required
               />
               <button className="--btn --btn-primary --btn-block">
-                Register
+                {t("register")}
               </button>
             </form>
             <span className={styles.register}>
-              <p>Already have an account? </p>
-              <Link to="/login">Login</Link>
+              <p>A{t("already-account")}</p>
+              <Link to="/login" className="login-right">
+                {t("login")}
+              </Link>
             </span>
           </div>
         </Card>
